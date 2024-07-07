f325fc2976b51d6bc4b498bdb9d8154e76b4647d
sahil parashar <79538902+iamsahilparashar@users.noreply.github.com>
2024-07-07 11:01:30 +0530

    Update request-methods.js

--- client/src/request-methods.js
+++ client/src/request-methods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://vercel.com/iamsahilparashars-projects/shopease_backend/api';
+const BASE_URL = 'https://shopease-backend.vercel.app/api';
 // const BASE_URL = 'https://ecom-back-sd62.onrender.com/api';
 const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMGEzOGUwYjczZTg5NTczZDAxNmZmNSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MzA3NTI3OSwiZXhwIjoxNjYzMTYxNjc5fQ.sLeBTfgAymobkEF1rcOzexB8-ixEXKWat-7lRFfaKVQ';
 
